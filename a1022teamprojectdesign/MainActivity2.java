566b7dcc763d77094cdb613140ff281b924fecf6
Hazm03 <99977107+Hazm03@users.noreply.github.com>
2022-03-16 18:18:10 -0400

    Update MainActivity2.java

--- a1022teamprojectdesign/MainActivity2.java
+++ a1022teamprojectdesign/MainActivity2.java
@@ -6,17 +6,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
-
+import android.widget.Button;
+import android.widget.EditText;
 import java.util.Random;
 
 public class MainActivity2 extends AppCompatActivity {
     int n;
     String textView3 = "";
+    TextView x3;
+    Button checkbutton;
     public void rng(){
         Random rand = new Random();
         n = rand.nextInt(100)+1;
     }
     public void guess(View view) {
+        updatecounter();
         EditText guess = (EditText) findViewById(R.id.editTextTextPersonName2);
         int guessint = Integer.parseInt(guess.getText().toString());
         if (n < guessint) {
@@ -27,6 +31,10 @@ public class MainActivity2 extends AppCompatActivity {
             textView3 = "Correct!";
         }
         Toast.makeText(this, textView3, Toast.LENGTH_SHORT).show();
+    }
+    private void updatecounter(){
+        counter ++;
+        x3.setText(counter + "");
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
