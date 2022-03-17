b1e227642da8e5f0b8851e8fee31a4a1d2415258
hojun0204 <99976174+hojun0204@users.noreply.github.com>
2022-03-16 21:14:24 -0400

    Update MainActivity3.java

--- a1022teamprojectdesign/MainActivity3.java
+++ a1022teamprojectdesign/MainActivity3.java
@@ -2,13 +2,29 @@ package com.example.a1022teamprojectdesign;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity3 extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        button = (Button) findViewById(R.id.nextbutton2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity4();
+            }
+        });
     }
-}
+        public void openActivity4() {
+            Intent intent = new Intent(this, MainActivity4.class);
+            startActivity(intent);
+        }
+    }
