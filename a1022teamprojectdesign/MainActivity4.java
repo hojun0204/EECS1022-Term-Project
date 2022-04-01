0075efd0b231bdd33ee598c3018c2f5133a768a5
hojun0204 <99976174+hojun0204@users.noreply.github.com>
2022-04-01 19:04:51 -0400

    Update MainActivity4.java

--- a1022teamprojectdesign/MainActivity4.java
+++ a1022teamprojectdesign/MainActivity4.java
@@ -2,13 +2,30 @@ package com.example.a1022teamprojectdesign;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity4 extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
+
+        button = (Button) findViewById(R.id.yesbutton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity2();
+            }
+        });
+    }
+    public void openActivity2() {
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
     }
 }
